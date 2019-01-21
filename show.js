5b4235e6723385057419664320facf756e3117d5
Kaung Htet Nyilay <kaunghtetnyilay@outlook.com>
2019-01-21 22:54:17 +0700

    add comments

--- show.js
+++ show.js
@@ -4,8 +4,11 @@ var image = document.getElementById ("image");
 
 var images = ["https://s3-us-west-2.amazonaws.com/s.cdpn.io/1259621/city.jpg", "https://s3-us-west-2.amazonaws.com/s.cdpn.io/1259621/cloudy.jpg", "https://s3-us-west-2.amazonaws.com/s.cdpn.io/1259621/green.jpg", "https://s3-us-west-2.amazonaws.com/s.cdpn.io/1259621/pretty_clouds.jpg"];
 
+//initiate index value
 var imageIndex = 0;
+//create click event for previous
 prev.onclick = function (){
+//if image index is 0, will pass to last image    
     if (imageIndex == 0) {
         imageIndex = images.length - 1;
     }
